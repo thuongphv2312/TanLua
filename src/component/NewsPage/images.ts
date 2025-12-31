fc2cef911e1381afe8e708c0af282ea28edb8d7e
BigMouse <tuyenphamthe139@gmail.com>
2025-12-31 15:22:41 +0700

    add img slider category

--- src/component/NewsPage/images.ts
+++ src/component/NewsPage/images.ts
@@ -14,11 +14,16 @@ export const TJ35_IMAGES = [
 ];
 
 export const SLIDER_IMAGES = [
-  "https://i.ibb.co/FLSQpQ48/G2TPL-F1.png",
-  "https://i.ibb.co/gZ9qzVVK/G2-TPL-F1-1.png",
-  "https://i.ibb.co/DfsjFHw8/G2-TPL-F1-2.png",
-  "https://i.ibb.co/jkrzQGM1/G2-TPL-F1-3.png",
-  "https://i.ibb.co/JR678FTp/G2-TPL-F1-4.png",
+  "https://i.ibb.co/Ps06LJp3/May-Nong-Nghiep.jpg",
+  "https://i.ibb.co/C5mgWtkZ/M-y-c-ng-nghi-p.jpg",
+  "https://i.ibb.co/wZhRKM43/D-ng-c-c-m-tay.jpg",
+  "https://i.ibb.co/ZRVpGgSC/Thi-t-b-x-t-r-a.png",
+  "https://i.ibb.co/hJNfM8N4/copy-of-may-phat-dien-chay-xang-elemax-5-0kva-sh6500exs.webp",
+  "https://i.ibb.co/ZpXCPdQQ/May-Can-Muc.webp",
+  "https://i.ibb.co/jk0fKZwL/tojiko-tj35-1.png",
+  "https://i.ibb.co/HLVk77sc/Mayxoidat.jpg",
+  "https://i.ibb.co/9kMHvYRg/may-bom-nuoc-4-thi-oshima-152f-2.jpg",
+  "https://i.ibb.co/Kj2zMYfr/may-nen-khi-oshima-2hp-30l-co-dau-truc-tiep-1-d2271544ec1043d0b6d7b9fa16b5f6e1.jpg",
 ];
 export const G2TPLF1_IMAGES = [
 "https://i.ibb.co/FLSQpQ48/G2TPL-F1.png",
@@ -44,6 +49,6 @@ export const CS226 = [
   "https://i.ibb.co/GvbPqGRH/z7060934406943-f9b1ab3dd7985f0b3cbe012e6294af9f.jpg",
   "https://i.ibb.co/Y7YzkTqY/z7060934406948-2cd7d1fecc44d8fa9b3bf21e943e0533.jpg",
   "https://i.ibb.co/YTRqZt0D/z7060934406905-96eb7eadc52da41f76e35bcec3775df3.jpg",
-"https://i.ibb.co/jk0V75sR/z7060934406952-238588ea08d6bb197c91e41709016e9a.jpg",
-"https://i.ibb.co/bRKgfnb4/z7060934406953-6e48e305ed557a4cd28640a26fb28e32.jpg"
-]
+  "https://i.ibb.co/jk0V75sR/z7060934406952-238588ea08d6bb197c91e41709016e9a.jpg",
+  "https://i.ibb.co/bRKgfnb4/z7060934406953-6e48e305ed557a4cd28640a26fb28e32.jpg",
+];
